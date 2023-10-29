420388bad7d703e6f19babf463ff9739e6197c38
Kenenisa Alemayehu <53818162+kenenisa@users.noreply.github.com>
2023-10-29 05:41:42 +0300

    added github link

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { Box, Button, Container, Stack, Typography } from '@mui/material'
+import { Box, Button, Container, IconButton, Stack, Typography } from '@mui/material'
 import Groups from './Components/Groups/Groups'
 import { useState } from 'react'
 import Brackets from './Components/Brackets/Brackets';
+import { Icon } from '@iconify/react/dist/iconify.js';
 
 function App() {
   const [page, setPage] = useState('group');
@@ -23,7 +24,14 @@ function App() {
   return (
     <Container maxWidth="xl">
       <Stack sx={{ mb: 7 }}>
-        <Typography variant='h3'>A2SV Champions League 2023</Typography>
+        <Stack direction="row" justifyContent="space-between">
+          <Typography variant='h3'>A2SV Champions League 2023</Typography>
+          <a href="https://github.com/kenenisa/acl" target='_blank'>
+            <IconButton sx={{ color: 'white', background: 'white' }} disableRipple>
+              <Icon icon="icon-park:github" />
+            </IconButton>
+          </a>
+        </Stack>
         <Box sx={{ my: 2 }}>
           <Button variant="outlined" sx={{ color: 'white' }} onClick={handlePageSwitch}>Switch to {reverse[page]}</Button>
         </Box>
